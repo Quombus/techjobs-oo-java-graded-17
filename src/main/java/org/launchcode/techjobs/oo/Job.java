9acf517e9c878e1805c13360e9f40cf9dc4f4715
Quombus <beckettrichardmatthew@gmail.com>
2024-10-14 16:05:10 -0500

    Added tests in JobClass that test the Job toString method

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -1,5 +1,9 @@
 package org.launchcode.techjobs.oo;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Job {
@@ -17,12 +21,12 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
-    public Job () {
+    public Job() {
         id = nextId;
         nextId++;
     }
 
-    public Job(String name, Employer employer, PositionType positionType, Location location, CoreCompetency coreCompetency) {
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
         this();
         this.name = name;
         this.employer = employer;
@@ -76,23 +80,50 @@ public class Job {
         return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public void setLocation(Location location) {
-        this.location = location;
-    }
+    public String toString() {
 
-    public void setEmployer(Employer employer) {
-        this.employer = employer;
-    }
+        String name = "";
+        String employer = "";
+        String location = "";
+        String position = "";
+        String competency = "";
+        String id = Integer.toString(this.getId());
 
-    public void setPositionType(PositionType positionType) {
-        this.positionType = positionType;
-    }
+        if (!(this.getName() == null || this.getName().isEmpty())) {
+            name = this.getName();
+        } else {
+            name = "Data Not Available";
+        }
 
-    public void setCoreCompetency(CoreCompetency coreCompetency) {
-        this.coreCompetency = coreCompetency;
+        if (!(this.getEmployer().toString() == null || this.getEmployer().toString().isEmpty())) {
+            employer = this.getEmployer().toString();
+        } else {
+            employer = "Data Not Available";
+        }
+
+        if (!(this.getLocation().toString() == null || this.getLocation().toString().isEmpty())) {
+            location = this.getLocation().toString();
+        } else {
+            location = "Data Not Available";
+        }
+        if (!(this.getPositionType().toString() == null || this.getPositionType().toString().isEmpty())) {
+            position = this.getPositionType().toString();
+        } else {
+            position = "Data Not Available";
+        }
+        if (!(this.getCoreCompetency().toString() == null || this.getCoreCompetency().toString().isEmpty())) {
+            competency = this.getCoreCompetency().toString();
+        } else {
+            competency = "Data Not Available";
+        }
+
+
+        return "\n" + "ID : " + id + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" + "Location: " + location + "\n" + "Position Type: " + position + "\n" + "Core Competency: " + competency + "\n";
+
+
     }
 }
+
+
+
--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -16,7 +17,7 @@ public class JobTest {
 
     @Test
     public void testJobConstructorSetsAllFields() {
-        Job testJob1 = new Job("Product tester", new Employer("ACME"), new PositionType("Quality control"), new Location("Desert"), new CoreCompetency("Persistence"));
+        Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         assertTrue(testJob1.getEmployer() instanceof Employer);
         assertTrue(testJob1.getPositionType() instanceof PositionType);
@@ -33,8 +34,48 @@ public class JobTest {
 
     @Test
     public void testJobsForEquality() {
-        Job testJob1 = new Job("Product tester", new Employer("ACME"), new PositionType("Quality control"), new Location("Desert"), new CoreCompetency("Persistence"));
-        Job testJob2 = new Job("Product tester", new Employer("ACME"), new PositionType("Quality control"), new Location("Desert"), new CoreCompetency("Persistence"));
+        Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         assertNotEquals(testJob1, testJob2);
     }
-}
+
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals('\n', testJob1.toString().charAt(0));
+        assertEquals('\n', testJob1.toString().charAt(testJob1.toString().length() - 1));
+    }
+
+    @Test
+    public void testToStringContainsCorrectLabelsAndData() {
+        Job testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        String id = Integer.toString(testJob1.getId());
+        String name = testJob1.getName();
+        String employer = testJob1.getEmployer().toString();
+        String location = testJob1.getLocation().toString();
+        String position = testJob1.getPositionType().toString();
+        String competency = testJob1.getCoreCompetency().toString();
+
+        assertEquals(testJob1.toString(), "\n" + "ID : " + id + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" + "Location: " + location + "\n" + "Position Type: " + position + "\n" + "Core Competency: " + competency + "\n");
+    }
+
+    @Test
+    public void testToStringHandlesEmptyField() {
+        Job testJob1 = new Job("Product tester", new Employer(""), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+
+        String id = Integer.toString(testJob1.getId());
+        String name = testJob1.getName();
+        String employer = "Data Not Available";
+        String location = testJob1.getLocation().toString();
+        String position = testJob1.getPositionType().toString();
+        String competency = testJob1.getCoreCompetency().toString();
+
+        assertEquals(testJob1.toString(), "\n" + "ID : " + id + "\n" + "Name: " + name + "\n" + "Employer: " + employer + "\n" + "Location: " + location + "\n" + "Position Type: " + position + "\n" + "Core Competency: " + competency + "\n");
+
+    }
+    }
+
+
